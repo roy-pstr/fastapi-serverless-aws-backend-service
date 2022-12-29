bac316cd5a7f7acacf8de2b8524f17dff4faa4a7
Roy Pasternak <roy.pasternakk@gmail.com>
2022-12-29 15:04:06 +0200

    add config file

---
+++ src/config.py
@@ -0,0 +1,26 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    """
+    Settings for the FastAPI server.
+    """
+
+    class Config:
+        """
+        Tell BaseSettings the env file path
+        """
+
+        env_file = ".env"
+
+
+@lru_cache()
+def get_settings(**kwargs):
+    """
+    Get settings. ready for FastAPI's Depends.
+    lru_cache - cache the Settings object per arguments given.
+    """
+    settings = Settings(**kwargs)
+    return settings
